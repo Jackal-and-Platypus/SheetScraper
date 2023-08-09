dffcedaacf2c899c25e0c05d901e32523d605e1f
mikazukiusaki <mikazukiyeh@gmail.com>
2023-08-09 17:17:28 +0800

    把我的爬蟲丟到新的分支

---
+++ scraper/gsheet_manager.py
@@ -0,0 +1,59 @@
+import os
+import pygsheets
+from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+load_dotenv()
+
+class SheetScraper:
+    def __init__(self, credentials_path, spreadsheet_key, sheet_name):
+        self.credentials_path = credentials_path
+        self.spreadsheet_key = spreadsheet_key
+        self.sheet_name = sheet_name
+
+    def get_steps(self):
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(self.credentials_path, scope)
+        client = gspread.authorize(credentials)
+
+        sheet = client.open_by_key(self.spreadsheet_key).worksheet(self.sheet_name)
+        data = sheet.get_all_records()
+
+        all_steps = []
+        current_step = None
+
+        for row in data:
+            step_number = row.get('step')
+            if isinstance(step_number, str) and step_number == "#":                
+                if current_step is not None:
+                    all_steps.append(current_step)
+                current_step = {
+                    "name": row.get('action_type'),
+                    "steps": []
+                }
+            else:
+                step_info = {
+                    "action_type": row.get('action_type'),
+                    "by_method": row.get('by_method'),
+                    "by_value": row.get('by_value'),
+                    "value": row.get('value')
+                }
+                if current_step is not None:
+                    current_step["steps"].append(step_info)
+
+        if current_step is not None:
+            all_steps.append(current_step)
+
+        return all_steps
+
+
+if __name__ == "__main__":
+    credentials_path = os.getenv("GOOGLE_SHEET_API_KEY_FILE")
+    spreadsheet_key = os.getenv("TEST_PLAN")
+    sheet_name = "suite_members"
+
+    scraper = SheetScraper(credentials_path, spreadsheet_key, sheet_name)
+    steps = scraper.get_steps()
+
+    print(steps)
