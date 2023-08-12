2d706a4ba72643819c257386bb63f55d19b9e28c
mikazukiusaki <mikazukiyeh@gmail.com>
2023-08-12 17:57:01 +0800

    依照issues改進程式嗎、並新增能取得單一 case 的 function

--- scraper/gsheet_manager.py
+++ scraper/gsheet_manager.py
@@ -4,25 +4,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class SheetScraper:
-    def __init__(self, credentials_path, spreadsheet_url, sheet_name):
+class GsheetManager:
+    def __init__(self, credentials_path, spreadsheet_url):
         self.credentials_path = credentials_path
         self.spreadsheet_url = spreadsheet_url
-        self.sheet_name = sheet_name
         self.client = self.authenticate()
 
     def authenticate(self):
         return pygsheets.authorize(service_file = self.credentials_path)
 
-    def get_steps(self):
+    def open_worksheet(self):
         spreadsheet = self.client.open_by_url(self.spreadsheet_url)
         sheet = spreadsheet.worksheet_by_title(self.sheet_name)
-        data = sheet.get_all_records()
+        return sheet
+
+    def set_sheet_name(self, sheet_name):
+        self.sheet_name = sheet_name
+
+    def get_records(self):
+        sheet = self.open_worksheet()
+        records = sheet.get_all_records()
+        return records
+
+    def get_steps(self):
+        records = self.get_records()
 
         all_steps = []
         current_step = None
 
-        for row in data:
+        for row in records:
             step_number = row.get('step')
             if step_number == "#":
                 if current_step is not None:
@@ -46,9 +56,7 @@ class SheetScraper:
         return all_steps
 
     def get_plan(self):
-        sheet = self.client.open_by_url(self.spreadsheet_url)
-        worksheet = sheet.worksheet_by_title(self.sheet_name)
-        records = worksheet.get_all_records()
+        records = self.get_records()
 
         test_plan = []
         for record in records:
@@ -63,18 +71,47 @@ class SheetScraper:
 
         return test_plan
 
+    def search_by_name(self, name):
+        steps = self.get_steps()
+
+        result = None
+        for step in steps:
+            if step["name"] == name:
+                result = step
+                break
+
+        return result
+
+def search_steps_by_name(credentials_path, spreadsheet_url, sheet_name, name):
+    scraper = GsheetManager(credentials_path, spreadsheet_url)
+    scraper.set_sheet_name(sheet_name)
+    result = scraper.search_by_name(name)
+    return result
+
 
 if __name__ == "__main__":
     credentials_path = os.getenv("GOOGLE_SHEET_API_KEY_FILE")
     spreadsheet_url = os.getenv("TEST_PLAN")
+    scraper = GsheetManager(credentials_path, spreadsheet_url)
+    
     sheet_name = "suite_members"
-    plan_sheet_name = "plan"
-
-    scraper = SheetScraper(credentials_path, spreadsheet_url, sheet_name)
+    scraper.set_sheet_name(sheet_name)
     steps = scraper.get_steps()
+    print("test_steps =")
+    print(steps)
 
 
-    scraper = SheetScraper(credentials_path, spreadsheet_url, plan_sheet_name)
+    plan_sheet_name = "plan"
+    scraper.set_sheet_name(plan_sheet_name)
     plan = scraper.get_plan()
+    print("test_plan =")
+    print(plan)
 
-
+    name_to_search = "刪除帳號"
+    sheet_name = "suite_members"
+    scraper.set_sheet_name(sheet_name)
+    search_result = scraper.search_by_name(name_to_search)
+    if search_result is None:
+        print("No matching content found")
+    else:
+        print(search_result)
