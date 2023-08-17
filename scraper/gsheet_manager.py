58808b263c0e39216540b2f26405abc543461462
prysline <mayhu1993@gmail.com>
2023-08-17 16:49:10 +0800

    feat: can update report to sheets

--- scraper/gsheet_manager.py
+++ scraper/gsheet_manager.py
@@ -4,14 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class GsheetManager:
     def __init__(self, credentials_path, spreadsheet_url):
+        self.sheet_name = ""
         self.credentials_path = credentials_path
         self.spreadsheet_url = spreadsheet_url
         self.client = self.authenticate()
 
     def authenticate(self):
-        return pygsheets.authorize(service_file = self.credentials_path)
+        return pygsheets.authorize(service_file=self.credentials_path)
 
     def open_worksheet(self):
         spreadsheet = self.client.open_by_url(self.spreadsheet_url)
@@ -25,6 +27,11 @@ class GsheetManager:
         sheet = self.open_worksheet()
         records = sheet.get_all_records()
         return records
+
+    def get_values(self):
+        sheet = self.open_worksheet()
+        values = sheet.get_all_values()
+        return values
 
     def get_steps(self):
         records = self.get_records()
@@ -42,13 +49,13 @@ class GsheetManager:
                     "steps": []
                 }
             elif current_step is not None and row.get('action_type') != "":
-                    step_info = {
-                        "action_type": row.get('action_type'),
-                        "by_method": row.get('by_method') or "None",
-                        "by_value": row.get('by_value') or "None",
-                        "value": row.get('value') or "None"
-                    }
-                    current_step["steps"].append(step_info)
+                step_info = {
+                    "action_type": row.get('action_type'),
+                    "by_method": row.get('by_method') or "None",
+                    "by_value": row.get('by_value') or "None",
+                    "value": row.get('value') or "None"
+                }
+                current_step["steps"].append(step_info)
 
         if current_step is not None:
             all_steps.append(current_step)
@@ -64,12 +71,18 @@ class GsheetManager:
                 "host": record["host"],
                 "suite": record["suite"],
                 "auth": record["auth"],
-                "snapshot": record["snapshot"],
-                "restart": record["restart"]
+                "snapshot": record["snapshot"]
             }
             test_plan.append(plan_entry)
 
         return test_plan
+
+    def update_case_result_to_col(self, case_reports, col):
+        sheet = self.open_worksheet()
+        for report in case_reports:
+            row = self.search_row_by_name(report["name"])
+            print(sheet.get_value((row, col)))
+            sheet.update_value((row, col), report["result"])
 
     def search_by_step_name(self, name):
         steps = self.get_steps()
@@ -92,12 +105,23 @@ class GsheetManager:
 
         return result
 
+    def search_row_by_name(self, name):
+        values = self.get_values()
+        row = 0
+
+        for value in values:
+            if value[0] == '#' and value[1] == name:
+                break
+            row += 1
+
+        return row + 1
+
 
 if __name__ == "__main__":
     credentials_path = os.getenv("GOOGLE_SHEET_API_KEY_FILE")
     spreadsheet_url = os.getenv("TEST_PLAN")
     scraper = GsheetManager(credentials_path, spreadsheet_url)
-    
+
     sheet_name = "suite_members"
     scraper.set_sheet_name(sheet_name)
     steps = scraper.get_steps()
@@ -114,3 +138,22 @@ if __name__ == "__main__":
     scraper.set_sheet_name(plan_sheet_name)
     search_result = scraper.search_by_plan_host(host_to_search)
 
+    scraper.set_sheet_name('test_case')
+    record = scraper.get_records()
+    row = scraper.search_row_by_name('auth_screenshot')
+
+    report = [{'name': 'index_RWD', 'result': 'success',
+               'report': [{'step': 3, 'action': 'check_css', 'value': '{"display":"none"}', 'result': 'success'},
+                          {'step': 5, 'action': 'check_css', 'value': '{"display":"block"}', 'result': 'success'}]},
+              {'name': 'auth_screenshot', 'result': 'success', 'report': [
+                  {'step': 1, 'action': 'check_link', 'value': '/backend/cms_project_detail.html?uuid=48821098',
+                   'result': 'success'},
+                  {'step': 2, 'action': 'check_link', 'value': '/backend/admin_agent_dashboard.html',
+                   'result': 'success'}]}]
+    # sheet = scraper.open_worksheet()
+    # print(sheet.get_all_values())
+    # print(sheet.get_all_records())
+
+    scraper.update_case_result_to_col(report, 6)
+
+    pass
